aff218d31587a353695bbf85e5dade0300d37e38
cherisechan <cherisechan@outlook.com.au>
2024-11-04 18:38:05 +1100

    added login and register route

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios';
+
+function Register() {
+    return (
+        <>
+            Register
+        </>
+    )
+}
+
+export default Register;
