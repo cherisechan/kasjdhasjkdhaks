512a98681b0fbaa23396953ef12e518994965208
cherisechan <cherisechan@outlook.com.au>
2024-11-07 02:26:52 +1100

    merged to main and changed functions to  arrow functions

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import axios from "axios";
 
-function Login() {
+const Login = () => {
     const navigate = useNavigate();
     useEffect(() => {
         if (localStorage.getItem("token")) {
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
-function Register() {
+const Register = () => {
     const navigate = useNavigate();
     useEffect(() => {
         if (localStorage.getItem("token")) {
