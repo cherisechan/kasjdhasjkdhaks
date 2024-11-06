8b0c284b52fad321dda03a89d3587eb7474598e5
Hannah Choi <z5406174@ad.unsw.edu.au>
2024-11-07 01:38:26 +1100

    added basics of landing page with navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import "./index.css"
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Register, Login } from "./pages";
+// import { Landing, Register, Login } from "./pages";
+import Landing from './pages/Landing';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
       </Routes>
