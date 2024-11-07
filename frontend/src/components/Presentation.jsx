0056604d406a9ec5aadcb189f914302f073feddd
cherisechan <cherisechan@outlook.com.au>
2024-11-07 16:29:05 +1100

    added presentation card componenet

---
+++ frontend/src/components/Presentation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Presentation = ({ name, thumbnail, description, numSlides }) => (
+    <div className="bg-violet-100 w-[30vw] h-[15vw] min-h-[150px] min-w-[300px] rounded-lg flex flex-col flex-shrink-0 overflow-auto m-2">
+        <div id="row-1" className="h-2/4 flex items-center mb-1">
+            <div className="px-2 py-2 h-full aspect-square">
+                {thumbnail ? (
+                    <img src={thumbnail} alt="thumbnail" className="object-cover w-full h-full" />
+                ) : (
+                    <div className="w-full h-full bg-gray-400"></div>
+                )}
+            </div>
+            <h2 className="text-5xl m-auto line-clamp-1 font-semibold ">{name}</h2>
+        </div>
+        <p className="text-gray-600 max-h-[30%] line-clamp-2 text-lg px-2 leading-tight">Description: {description}</p>
+        <p className="text-sm text-gray-500 px-2 mt-auto w-min pb-2">Slides: {numSlides}</p>
+    </div>
+);
+
+export default Presentation;
