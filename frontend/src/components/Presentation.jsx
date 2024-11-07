6b839e3399cd75f9b0b9f8fb3e37dd8d3cc38602
cherisechan <cherisechan@outlook.com.au>
2024-11-07 16:50:31 +1100

    added responsiveness of line clamp in presentation

--- frontend/src/components/Presentation.jsx
+++ frontend/src/components/Presentation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Presentation = ({ name, thumbnail, description, numSlides }) => (
-    <div className="bg-violet-100 w-[30vw] h-[15vw] min-h-[150px] min-w-[300px] rounded-lg flex flex-col flex-shrink-0 overflow-auto m-2">
+    <div className="bg-violet-100 w-[30vw] h-[15vw] min-h-[150px] min-w-[300px] rounded-lg flex flex-col flex-shrink-0 overflow-auto">
         <div id="row-1" className="h-2/4 flex items-center mb-1">
             <div className="px-2 py-2 h-full aspect-square">
                 {thumbnail ? (
@@ -12,8 +12,8 @@ const Presentation = ({ name, thumbnail, description, numSlides }) => (
             </div>
             <h2 className="text-5xl m-auto line-clamp-1 font-semibold ">{name}</h2>
         </div>
-        <p className="text-gray-600 max-h-[30%] line-clamp-2 text-lg px-2 leading-tight">Description: {description}</p>
-        <p className="text-sm text-gray-500 px-2 mt-auto w-min pb-2">Slides: {numSlides}</p>
+        <p className="text-gray-600 max-h-[30%] xl:line-clamp-3 line-clamp-2 text-lg px-2 leading-tight">Description: {description}</p>
+        <p className="text-sm text-gray-500 px-1 mt-auto w-min pb-1">Slides: {numSlides}</p>
     </div>
 );
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -13,14 +13,16 @@ const Dashboard = () => {
         <>
             <div id="dashboard" className="flex w-screen h-screen justify-center items-center bg-violet-200">
                 <button className="absolute top-1  bg-violet-700 hover:bg-violet-500 text-white font-bold py-2 px-4 rounded my-2">CREATE</button>
-                <div className="overflow-auto max-w-full flex flex-wrap max-h-[75%] flex-row margin-auto justify-center">                    <Presentation name="hi" description="hihihihihihi hihih hihihi hi hi hi hi hi h ih i hi hi hi hi xxxxxxxxx asa a as a a a a asda sd asd as dsa asd asd asd asd asd sad asd asd sdsdsdsdsadjkah ajsdhsias asua sgdasdg"></Presentation>
+                <div className="overflow-auto max-w-full flex flex-wrap max-h-[75%] flex-row margin-auto justify-start pl-[3vw] gap-3">
+                    <Presentation name="hi" description="hi gg hihi hihih hihihi hi hi hi higg  hi h ih i hi hi hi hi xxxxxxxggxx asa a as a a a a asda sd asd as dsa asd asd asd asd asd sad asd asd sdsdsdsdsadjkah ajsdhsias asua sgdasdg"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
                     <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
-                    <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>  
+                    <Presentation name="hi" description="hihihihihihi hihih hihihi hi"></Presentation>
+  
                 </div>
             </div>
         </>
