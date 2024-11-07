e8751c9e31650b05c305077842e82b6d75e8639e
Hannah Choi <z5406174@ad.unsw.edu.au>
2024-11-08 00:49:05 +1100

    removed buttons in navbar on login and register pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Navbar = ({ isLoggedIn, onLogout }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
     onLogout();
     navigate('/');
   };
+
+  // hide buttons on navbar on login and register page
+  const showButtons = location.pathname !== '/login' && location.pathname !== '/register';
   
   return (
     <div className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <div className="text-2xl font-bold text-white">Presto</div>
-        <div className="flex space-x-4">
-          {isLoggedIn ? (
-            <button onClick={handleLogout} className="bg-red-500 text-white hover:bg-red-700 font-bold py-2 px-4 rounded">Logout</button>
-          ) : (
-            <>
-              <button onClick={() => navigate('/login')} className="text-white bg-violet-700 hover:bg-violet-500 font-bold py-2 px-4 rounded">Log In</button>
-              <button onClick={() => navigate('/register')} className="text-white bg-violet-700 hover:bg-violet-500 font-bold py-2 px-4 rounded">Register</button>
-            </>
-          )}
-        </div>
+        {showButtons && (
+          <div className="flex space-x-4">
+            {isLoggedIn ? (
+              <button onClick={handleLogout} className="bg-red-500 text-white hover:bg-red-700 font-bold py-2 px-4 rounded">Logout</button>
+            ) : (
+              <>
+                <button onClick={() => navigate('/login')} className="text-white bg-violet-700 hover:bg-violet-500 font-bold py-2 px-4 rounded">Log In</button>
+                <button onClick={() => navigate('/register')} className="text-white bg-violet-700 hover:bg-violet-500 font-bold py-2 px-4 rounded">Register</button>
+              </>
+            )}
+          </div>
+        )}
       </div>
     </div>
   )
