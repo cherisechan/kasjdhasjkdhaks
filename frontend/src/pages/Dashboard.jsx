8a93b1d9091eb9411e3d741ddfcc62d71942fd75
cherisechan <cherisechan@outlook.com.au>
2024-11-08 15:22:02 +1100

    added create presentation modal

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,30 +2,55 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import axios from "axios";
 import Presentation from "../components/Presentation"
+import CreateModal from "../components/CreateModal"
 
 const Dashboard = () => {
+    const [token, setToken] = useState(null);
+    const [showCreate, setShowCreate] = useState(false);
+    const [presentations, setPresentations] = useState(null);
     const navigate = useNavigate();
     useEffect(() => {
-        if (!localStorage.getItem("token")) {
+        const getToken = localStorage.getItem("token");
+        if (!getToken) {
             navigate("/login");
+        } else {
+            setToken(getToken);
         }
-    }, []);
+    }, [navigate]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!token) return;
+            const headers = {
+                headers: {
+                    'accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            };
+            const store = await axios.get("http://localhost:5005/store", headers);
+            setPresentations(store.data);
+        };
+        fetchData();
+    }, [token, showCreate]);
+
+
+
 
     return (
         <>
+            {showCreate ? (<CreateModal setShowCreate={setShowCreate}/>) : (<></>)}
             <div id="dashboard" className="flex flex-col w-screen h-screen justify-start items-center bg-gradient-to-b from-violet-500 to-violet-300">
-                <button className="mt-28 mb-6 mx-14 self-start text-xl bg-violet-700 hover:bg-violet-300 text-white font-bold py-3 px-5  rounded my-2">CREATE</button>
+                <button className="mt-28 mb-6 mx-14 self-start text-xl bg-violet-700 hover:bg-violet-300 text-white font-bold py-3 px-5  rounded my-2" onClick={() => setShowCreate(true)}>CREATE</button>
                 <div className="overflow-auto max-w-full grid grid-cols-3 max-warps:grid-cols-2  max-sm:grid-cols-1 max-h-[70%] gap-3 px-3 scrollbar scrollbar-w-2 scrollbar-thumb-violet-300 scrollbar-track-gray-3  00 scrollbar-thumb-rounded">
-                    <Presentation name="hi" description="hi gg hihi hihih hihihi hi hi hi higg  hi h ih i hi hi hi hi xxxxxxxggxx asa a as a a a a asda sd asd as dsa asd asd asd asd asd sad asd asd sdsdsdsdsadjkah ajsdhsias asua sgdasdg"></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
-                    <Presentation name="hi" description=""></Presentation>
+                {presentations?.store && [...presentations.store].reverse().map((presentation, index) => (
+                    <Presentation
+                        key={index}
+                        name={presentation.name}
+                        description={presentation.description}
+                        numSlides={presentation.slides.length}
+                    />
+                ))}
                 </div>
             </div>
         </>
