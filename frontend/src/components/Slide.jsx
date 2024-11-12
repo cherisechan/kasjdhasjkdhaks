f9814012247f81fefa345643c49e4a962a844e21
Hannah Choi <z5406174@ad.unsw.edu.au>
2024-11-12 18:17:00 +1100

    modified slide component to accommodate video elements

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -1,8 +1,10 @@
 import SlideBase from "./SlideBase";
 import TextElement from "./TextElement";
 import ImageElement from "./ImageElement";
+import VideoElement from "./VideoElement";
 import TextEditModal from "./TextEditModal";
 import ImageEditModal from "./ImageEditModal"; 
+import VideoEditModal from "./VideoEditModal";
 import { useState, useEffect } from "react";
 
 const Slide = ({ slide, currIndex, setUpdateObj, setUpdateElemId }) => {
@@ -33,7 +35,7 @@ const Slide = ({ slide, currIndex, setUpdateObj, setUpdateElemId }) => {
         }
     };
 
-    // State for image editing
+    // for image edit
     const [showImageEditModal, setShowImageEditModal] = useState(false);
     const [imageEditSubmit, setImageEditSubmit] = useState(false);
     const [imageElem, setImageElem] = useState(null);
@@ -50,6 +52,26 @@ const Slide = ({ slide, currIndex, setUpdateObj, setUpdateElemId }) => {
         setElemId(e.target.id);
         if (e.target.id) {
             setShowImageEditModal(true);
+        }
+    };
+
+    // for video edit
+    const [showVideoEditModal, setShowVideoEditModal] = useState(false);
+    const [videoEditSubmit, setVideoEditSubmit] = useState(false);
+    const [videoElem, setVideoElem] = useState(null);
+
+    useEffect(() => {
+        if (videoEditSubmit && videoElem) {
+            setUpdateObj(videoElem);
+            setUpdateElemId(elemId);
+            setVideoEditSubmit(false);
+        }
+    }, [videoEditSubmit, videoElem]);
+
+    const openVideoEdit = (e) => {
+        setElemId(e.target.id);
+        if (e.target.id) {
+            setShowVideoEditModal(true);
         }
     };
 
@@ -73,20 +95,32 @@ const Slide = ({ slide, currIndex, setUpdateObj, setUpdateElemId }) => {
                 />
             )}
 
+            {showVideoEditModal && (
+                <VideoEditModal
+                    element={slide.elements.find((e) => e.id === elemId)}
+                    setVideoElem={setVideoElem}
+                    setVideoEditSubmit={setVideoEditSubmit}
+                    setShowVideoEditModal={setShowVideoEditModal}
+                />
+            )}
+
             {/* Slide Content */}
             <SlideBase $bgColour1={slide.background.colour1} $bgColour2={slide.background.colour2} $gradient={slide.background.gradient} $bgImg={slide.background.img} id={slide.id}>
                 {elements.map((t, index) => {
-                if (t.type === "text") {
-                    return (
-                        <TextElement id={t.id} $textObj={t} text={t.text} openTextEdit={openTextEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
-                    );
-                } else if (t.type === "image") {
-                    return (
-                        <ImageElement id={t.id} $imageObj={t} openImageEdit={openImageEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
-                    );
-                } else {
+                    if (t.type === "text") {
+                        return (
+                            <TextElement id={t.id} $textObj={t} text={t.text} openTextEdit={openTextEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
+                        );
+                    } else if (t.type === "image") {
+                        return (
+                            <ImageElement id={t.id} $imageObj={t} openImageEdit={openImageEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
+                        );
+                    } else if (t.type === "video") {
+                        return (
+                            <VideoElement id={t.id} $videoObj={t} openVideoEdit={openVideoEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
+                        );
+                    }
                     return null;
-                }
                 })}
                 <p className="absolute text-center text-gray-600 bottom-2 left-3 text-[1em] z-[900]">{currIndex + 1}</p>
             </SlideBase>
