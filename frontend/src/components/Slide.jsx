a58946a1354fab4a3a3c6f6cf22be2069412e4a4
Hannah Choi <z5406174@ad.unsw.edu.au>
2024-11-12 16:45:29 +1100

    updated slide.jsx to handle image edit modal

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -1,59 +1,97 @@
 import SlideBase from "./SlideBase";
 import TextElement from "./TextElement";
+import ImageElement from "./ImageElement";
 import TextEditModal from "./TextEditModal";
+import ImageEditModal from "./ImageEditModal"; 
 import { useState, useEffect } from "react";
+
 const Slide = ({ slide, currIndex, setUpdateObj, setUpdateElemId }) => {
     let elements = slide.elements;
     elements = elements.map((e, index) => ({
         ...e,
         z: `${500 - parseInt(index)}`,
-    }))
+    }));
 
     // for text edit
     const [showTextEditModal, setShowTextEditModal] = useState(false);
     const [elemId, setElemId] = useState("");
     const [textEditSubmit, setTextEditSubmit] = useState(false);
     const [textElem, setTextElem] = useState(null);
+
     useEffect(() => {
         if (textEditSubmit && textElem) {
             setUpdateObj(textElem);
-            setUpdateElemId(elemId)
+            setUpdateElemId(elemId);
             setTextEditSubmit(false);
         }
-    }, [textEditSubmit])
+    }, [textEditSubmit, textElem]);
 
-    const openTextEdit = e => {
+    const openTextEdit = (e) => {
         setElemId(e.target.id);
         if (e.target.id) {
             setShowTextEditModal(true);
         }
-    }
+    };
+
+    // State for image editing
+    const [showImageEditModal, setShowImageEditModal] = useState(false);
+    const [imageEditSubmit, setImageEditSubmit] = useState(false);
+    const [imageElem, setImageElem] = useState(null);
+
+    useEffect(() => {
+        if (imageEditSubmit && imageElem) {
+            setUpdateObj(imageElem);
+            setUpdateElemId(elemId);
+            setImageEditSubmit(false);
+        }
+    }, [imageEditSubmit, imageElem]);
+
+    const openImageEdit = (e) => {
+        setElemId(e.target.id);
+        if (e.target.id) {
+            setShowImageEditModal(true);
+        }
+    };
 
     return (
         <>
             {showTextEditModal && (
                 <TextEditModal
-                    element={slide.elements.find(e => e.id === elemId)}
+                    element={slide.elements.find((e) => e.id === elemId)}
                     setTextElem={setTextElem}
                     setTextEditSubmit={setTextEditSubmit}
                     setShowTextEditModal={setShowTextEditModal}
                 />
             )}
-            <SlideBase $bgColour1={slide.background.colour1} $bgColour2={slide.background.colour2} $gradient={slide.background.gradient} $bgImg={slide.background.img} id={slide.id} >
-                {
-                    elements.map((t, index) =>{
-                        if (t.type === "text") {
-                            return (
-                                <TextElement id={slide.elements[index].id} className="text-element hover:cursor-pointer" $textObj={t} text={t.text} openTextEdit={openTextEdit} key={index} />
-                            )
-                        }
-                        
-                    })
+
+            {showImageEditModal && (
+                <ImageEditModal
+                    element={slide.elements.find((e) => e.id === elemId)}
+                    setImageElem={setImageElem}
+                    setImageEditSubmit={setImageEditSubmit}
+                    setShowImageEditModal={setShowImageEditModal}
+                />
+            )}
+
+            {/* Slide Content */}
+            <SlideBase $bgColour1={slide.background.colour1} $bgColour2={slide.background.colour2} $gradient={slide.background.gradient} $bgImg={slide.background.img} id={slide.id}>
+                {elements.map((t, index) => {
+                if (t.type === "text") {
+                    return (
+                        <TextElement id={t.id} $textObj={t} text={t.text} openTextEdit={openTextEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
+                    );
+                } else if (t.type === "image") {
+                    return (
+                        <ImageElement id={t.id} $imageObj={t} openImageEdit={openImageEdit} key={t.id} setUpdateObj={setUpdateObj} setUpdateElemId={setUpdateElemId}/>
+                    );
+                } else {
+                    return null;
                 }
+                })}
                 <p className="absolute text-center text-gray-600 bottom-2 left-3 text-[1em] z-[900]">{currIndex + 1}</p>
             </SlideBase>
         </>
-    )
-}
+    );
+};
 
 export default Slide;
