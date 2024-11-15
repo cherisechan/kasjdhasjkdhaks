736a7db1377582d03499833319d045d61d6ef199
cherisechan <cherisechan@outlook.com.au>
2024-11-15 17:07:22 +1100

    linted

--- frontend/src/components/ImageElement.jsx
+++ frontend/src/components/ImageElement.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Rnd } from "react-rnd";
 
