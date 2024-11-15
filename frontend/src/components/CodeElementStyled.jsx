f4607a2b207614a4dc019d5698e4996211e30178
cherisechan <cherisechan@outlook.com.au>
2024-11-15 17:03:45 +1100

    linted

--- frontend/src/components/CodeElementStyled.jsx
+++ frontend/src/components/CodeElementStyled.jsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 const CodeElementStyled = styled.div(({$codeObj, readOnly}) => ({
-    position: "relative",
-    height: '100%',
-    width: '100%',
-    fontSize: `${$codeObj.fontSize}em`,
-    overflow: "hidden",
-    // top: `${$codeObj.x}%`,
-    // left: `${$codeObj.y}%`,
-    // zIndex: `${$codeObj.z}`,
-    // border: "1px solid #d3d3d3",
-    border: readOnly ? 'none' : '1px solid #d3d3d3',
-    backgroundColor: "#2d2d2d",
+  position: "relative",
+  height: '100%',
+  width: '100%',
+  fontSize: `${$codeObj.fontSize}em`,
+  overflow: "hidden",
+  // top: `${$codeObj.x}%`,
+  // left: `${$codeObj.y}%`,
+  // zIndex: `${$codeObj.z}`,
+  // border: "1px solid #d3d3d3",
+  border: readOnly ? 'none' : '1px solid #d3d3d3',
+  backgroundColor: "#2d2d2d",
 }));
 
 export default CodeElementStyled;
